82c206f5fd37bf44de64ef1bbdfd6c9c591f3575
Vadim <vadim.gerasko99@gmail.com>
2021-09-23 17:48:11 +0600

    first commit, add HTag and start create component Button

---
+++ components/index.ts
@@ -0,0 +1,4 @@
+
+
+export * from "./HTag/HTag"
+export * from "./Button/Button"
---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Button, Htag } from '../components'
+import styles from '../styles/Home.module.css'
+
+export default function Home(): JSX.Element {
+  return (
+    <>
+      <Htag tag="h1">Текст</Htag>
+      <Button appearance="primaty">Кнопка</Button>
+      <Button appearance="ghost">Кнопка</Button>
+    </>
+  )
+}
